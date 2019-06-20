d70b46848db8488cef384b6ecbd6898ffb075421
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-20 15:56:05 +0200

    Project flask with sql terminated

--- schemas.py
+++ schemas.py
@@ -6,7 +6,7 @@ from models import Product
 class ProductSchema(ma.Schema):
     class Meta:
         model = Product
-        fields = ('id', 'name') # These are the fields we want in the JSON!
+        fields = sorted(('id', 'name', 'description'), reverse=False) # These are the fields we want in the JSON!
 
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, request
 from config import Config
 
 
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
 
 @app.route('/hello')
@@ -28,3 +28,32 @@ def hello():
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    product = db.session.query(Product).get(id) # SQLAlchemy request => 'SELECT * FROM products WHERE products.id = "1"'
+    return product_schema.jsonify(product)
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    body = request.get_json()
+    product = Product(name=body['name'])
+    db.session.add(product) # SQLAlchemy request => 'INSERT values'
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = db.session.query(Product).get(id)
+    db.session.delete(product) # SQLAlchemy request => 'DELETE values'
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+@app.route('/products', methods=['PATCH'])
+def update_product():
+    body = request.get_json()
+    product = db.session.query(Product).get(body['id'])
+    product.name = body['name']
+    product.description = body['description']
+    db.session.commit()
+    return product_schema.jsonify(product)
