35ff46b9e784ea0b9d0def6829e939a9e31cbd52
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-21 15:24:05 +0200

    Add Celery and configure Heroku

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ["0SmGhlE2E7cKxztXCR3u7w"]
     CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
     CELERY_BROKER_URL = os.environ['REDIS_URL']
